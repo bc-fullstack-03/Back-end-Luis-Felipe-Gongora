f635949d170ff8c990bf9e3174e9fc28eed2bb89
Luis Felipe G. Garcia <lfelipeggarcia@gmail.com>
2023-04-29 23:04:39 -0300

    Starter project

---
+++ src/main/java/com/showus/showus/ShowUsApplication.java
@@ -0,0 +1,13 @@
+package com.showus.showus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showus/showus/ShowUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showus.showus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
